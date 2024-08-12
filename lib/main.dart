b4d91056a7e0c3e286ad1a11eb0b768a7d505bb9
Asheesh Kumar <asheesh220720@gmail.com>
2024-08-12 13:20:16 +0530

    inital commit

---
+++ lib/main.dart
@@ -0,0 +1,17 @@
+import 'package:api_tutorial/home_screen.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(context) {
+    return MaterialApp(
+      home: HomeScreen(),
+    );
+  }
+}

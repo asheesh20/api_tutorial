937e4631fb7047c139663560304577db728eb10a
Asheesh Kumar <asheesh220720@gmail.com>
2024-08-14 19:50:57 +0530

    Example Three with User Model

--- lib/views/example_three.dart
+++ lib/views/example_three.dart
@@ -11,25 +11,41 @@ class ExampleThree extends StatefulWidget {
   State<ExampleThree> createState() => _ExampleThreeState();
 }
 
-List<ExampleThreeModel> allList = [];
+// List<ExampleThreeModel> allList = [];
 
-Future<List<ExampleThreeModel>> getData() async {
+// Future<List<ExampleThreeModel>> getData() async {
+//   final response =
+//       await http.get(Uri.parse('https://jsonplaceholder.typicode.com/users'));
+//   final data = jsonDecode(response.body.toString());
+//   if (response.statusCode == 200) {
+//     for (Map i in data) {
+//       allList.add(ExampleThreeModel(
+//           id: i['id'],
+//           name: i['name'],
+//           username: i['username'],
+//           email: i['email'],
+//           phone: i['phone'],
+//           website: i['website']));
+//     }
+//     return allList;
+//   } else {
+//     return allList;
+//   }
+// }
+
+List<UserModel> userList = [];
+
+Future<List<UserModel>> getUser() async {
   final response =
       await http.get(Uri.parse('https://jsonplaceholder.typicode.com/users'));
   final data = jsonDecode(response.body.toString());
   if (response.statusCode == 200) {
     for (Map i in data) {
-      allList.add(ExampleThreeModel(
-          id: i['id'],
-          name: i['name'],
-          username: i['username'],
-          email: i['email'],
-          phone: i['phone'],
-          website: i['website']));
+      userList.add(UserModel.fromJson(i));
     }
-    return allList;
+    return userList;
   } else {
-    return allList;
+    return userList;
   }
 }
 
@@ -42,22 +58,77 @@ class _ExampleThreeState extends State<ExampleThree> {
       ),
       body: Column(
         children: [
+          // Expanded(
+          //   child: FutureBuilder(
+          //       future: getData(),
+          //       builder: (context, snapshot) {
+          //         if (!snapshot.hasData) {
+          //           return const Center(child: CircularProgressIndicator());
+          //         } else {
+          //           return ListView.builder(
+          //               itemCount: allList.length,
+          //               itemBuilder: (context, index) {
+          //                 return Padding(
+          //                   padding: const EdgeInsets.all(8.0),
+          //                   child: Card(
+          //                     elevation: 0,
+          //                     child: ListTile(
+          //                       title: Text(allList[index].name.toString()),
+          //                     ),
+          //                   ),
+          //                 );
+          //               });
+          //         }
+          //       }),
+          // ),
           Expanded(
             child: FutureBuilder(
-                future: getData(),
+                future: getUser(),
                 builder: (context, snapshot) {
                   if (!snapshot.hasData) {
                     return const Center(child: CircularProgressIndicator());
                   } else {
                     return ListView.builder(
-                        itemCount: allList.length,
+                        itemCount: userList.length,
                         itemBuilder: (context, index) {
                           return Padding(
                             padding: const EdgeInsets.all(8.0),
                             child: Card(
                               elevation: 0,
-                              child: ListTile(
-                                title: Text(allList[index].name.toString()),
+                              child: Padding(
+                                padding: const EdgeInsets.all(8.0),
+                                child: Column(
+                                  children: [
+                                    ReuseableRow(
+                                      title: 'Name',
+                                      value: userList[index].name.toString(),
+                                    ),
+                                    ReuseableRow(
+                                      title: 'UserName',
+                                      value:
+                                          userList[index].username.toString(),
+                                    ),
+                                    ReuseableRow(
+                                      title: 'Email',
+                                      value: userList[index].email.toString(),
+                                    ),
+                                    ReuseableRow(
+                                      title: 'Address',
+                                      value: userList[index]
+                                          .address!
+                                          .city
+                                          .toString(),
+                                    ),
+                                    ReuseableRow(
+                                      title: 'Latitude',
+                                      value: userList[index]
+                                          .address!
+                                          .geo!
+                                          .lat
+                                          .toString(),
+                                    ),
+                                  ],
+                                ),
                               ),
                             ),
                           );
@@ -65,6 +136,25 @@ class _ExampleThreeState extends State<ExampleThree> {
                   }
                 }),
           ),
+        ],
+      ),
+    );
+  }
+}
+
+class ReuseableRow extends StatelessWidget {
+  String title, value;
+  ReuseableRow({super.key, required this.title, required this.value});
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.all(8.0),
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        children: [
+          Text(title),
+          Text(value),
         ],
       ),
     );
